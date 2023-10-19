26990d9ef6547c2df9e761880247e4e1a8895cf6
sunnyle13 <4anythingbutwork@gmail.com>
2023-10-19 04:28:03 -0400

    Set up routing to view game details

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import router from "./services/routes";
 import theme from "./theme";
 
 const queryClient = new QueryClient();
@@ -13,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
